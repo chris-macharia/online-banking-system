1e787637fa508bcdd7f667a42fc7f92f8f166666
chris-macharia <christophermaina003@gmail.com>
2022-12-12 09:57:31 +0300

    Create new user, Prompt user

--- src/online-banking-system.c
+++ src/online-banking-system.c
@@ -10,8 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-	puts("Hello-eclipse"); /* prints !!!Hello World!!! */
+struct user
+{
+	char phone[50];
+	char account[50];
+	char password[50];
+	float balance;
+};
+
+int main(void)
+{
+	struct user usr;
+
+	printf("\n Please select one of the above: ");
+	printf("\n\n1.Register an account.");
+	printf("\n2.Login to an account.");
 	return EXIT_SUCCESS;
 }
