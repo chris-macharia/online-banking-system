1075219925ebca6561168852f55192a863c4b75d
chris-macharia <christophermaina003@gmail.com>
2022-12-12 07:14:05 +0300

    Intital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ONLINE BANKING SYSTEM  
+This project will take you through creating an online banking system.  
+HAPPY CODING!!
---
+++ src/online-banking-system.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : online-banking-system.c
+ Author      : chris-macharia
+ Version     :
+ Copyright   : free-software!!
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello-eclipse"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
